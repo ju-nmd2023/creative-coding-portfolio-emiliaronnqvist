ecb207de3bb6f4759a1a28e49afde9b1cbd6ab11
emiliaronnqvist <emilia.ronnqvist@hotmail.com>
2025-09-27 09:37:36 +0200

    .

--- experiments/music.js
+++ experiments/music.js
@@ -1,122 +1,24 @@
-const sineButton = document.getElementById("sine");
-const startButton = document.getElementById("start");
-const stopButton = document.getElementById("stop");
-const attackInput = document.getElementById("attack");
-const decayInput = document.getElementById("decay");
-const sustainInput = document.getElementById("sustain");
-const releaseInput = document.getElementById("release");
-const buttonC = document.getElementById("buttonC");
-const buttonD = document.getElementById("buttonD");
-const buttonE = document.getElementById("buttonE");
-const buttonF = document.getElementById("buttonF");
-const buttonG = document.getElementById("buttonG");
-const buttonA = document.getElementById("buttonA");
-const buttonB = document.getElementById("buttonB");
-const buttonC2 = document.getElementById("buttonC2");
-const frequencyInput = document.getElementById("frequency");
-const delayTimeInput = document.getElementById("delayTime");
-const depthInput = document.getElementById("depth");
+//This code is adapted and inspired by https://codepen.io/pixelkind/pen/RwEVVaw
 
-let chorus;
 let synth;
 
+//Following one line was adapted from https://chatgpt.com/share/68d51069-73bc-8005-ac1d-0861224e6c6c
+const noteArray = ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4"];
+
 window.addEventListener("load", () => {
-  // Create Chorus effect and start
-  chorus = new Tone.Chorus(4, 2.5, 0.5).toDestination().start();
-  //Create polySynth
-  synth = new Tone.PolySynth().connect(chorus);
-  // Set oscilliator to Sine
+  synth = new Tone.PolySynth().toDestination();
   synth.set({
     oscillator: {
       type: "sine",
     },
   });
-});
-
-sineButton.addEventListener("click", () => {
-  synth.set({
-    oscillator: {
-      type: "sine",
-    },
+  window.addEventListener("click", async () => {
+    await Tone.start();
+    console.log("started");
   });
-});
-
-attackInput.addEventListener("change", () => {
-  synth.set({
-    envelope: {
-      attack: attackInput.value,
-    },
+  window.addEventListener("click", () => {
+    //Following 2 lines was adapted from https://chatgpt.com/share/68d51069-73bc-8005-ac1d-0861224e6c6c
+    const randomNote = noteArray[Math.floor(Math.random() * noteArray.length)];
+    synth.triggerAttackRelease(randomNote, "4n");
   });
-});
-
-decayInput.addEventListener("change", () => {
-  synth.set({
-    envelope: {
-      decay: decayInput.value,
-    },
-  });
-});
-
-sustainInput.addEventListener("change", () => {
-  synth.set({
-    envelope: {
-      sustain: sustainInput.value,
-    },
-  });
-});
-
-releaseInput.addEventListener("change", () => {
-  synth.set({
-    envelope: {
-      release: releaseInput.value,
-    },
-  });
-});
-
-frequencyInput.addEventListener("change", () => {
-  chorus.frequency.value = frequencyInput.value;
-});
-
-delayTimeInput.addEventListener("change", () => {
-  chorus.delayTime = delayTimeInput.value;
-});
-
-depthInput.addEventListener("change", () => {
-  chorus.depth = depthInput.value;
-});
-
-buttonC.addEventListener("click", () => {
-  synth.triggerAttackRelease(["C3", "E3"], "4n");
-});
-
-buttonD.addEventListener("click", () => {
-  synth.triggerAttackRelease(["D3", "F3"], "4n");
-});
-
-buttonE.addEventListener("click", () => {
-  synth.triggerAttackRelease(["E3", "G3"], "4n");
-});
-
-buttonF.addEventListener("click", () => {
-  synth.triggerAttackRelease(["F3", "A3"], "4n");
-});
-
-buttonG.addEventListener("click", () => {
-  synth.triggerAttackRelease(["G3", "B3"], "4n");
-});
-
-buttonA.addEventListener("click", () => {
-  synth.triggerAttackRelease(["A3", "C4"], "4n");
-});
-
-buttonB.addEventListener("click", () => {
-  synth.triggerAttackRelease(["B3", "D4"], "4n");
-});
-
-buttonC2.addEventListener("click", () => {
-  synth.triggerAttackRelease(["C4", "E4"], "4n");
-});
-
-window.addEventListener("click", () => {
-  Tone.start().then(() => console.log("audio started"));
 });
--- experiments/version6.js
+++ experiments/version6.js
@@ -112,3 +112,128 @@ function draw() {
     agent.draw();
   }
 }
+
+//music
+const sineButton = document.getElementById("sine");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const attackInput = document.getElementById("attack");
+const decayInput = document.getElementById("decay");
+const sustainInput = document.getElementById("sustain");
+const releaseInput = document.getElementById("release");
+const buttonC = document.getElementById("buttonC");
+const buttonD = document.getElementById("buttonD");
+const buttonE = document.getElementById("buttonE");
+const buttonF = document.getElementById("buttonF");
+const buttonG = document.getElementById("buttonG");
+const buttonA = document.getElementById("buttonA");
+const buttonB = document.getElementById("buttonB");
+const buttonC2 = document.getElementById("buttonC2");
+const frequencyInput = document.getElementById("frequency");
+const delayTimeInput = document.getElementById("delayTime");
+const depthInput = document.getElementById("depth");
+
+let chorus;
+let synth;
+
+window.addEventListener("load", () => {
+  // Create Chorus effect and start
+  chorus = new Tone.Chorus(4, 2.5, 0.5).toDestination().start();
+  //Create polySynth
+  synth = new Tone.PolySynth().connect(chorus);
+  // Set oscilliator to Sine
+  synth.set({
+    oscillator: {
+      type: "sine",
+    },
+  });
+});
+
+sineButton.addEventListener("click", () => {
+  synth.set({
+    oscillator: {
+      type: "sine",
+    },
+  });
+});
+
+attackInput.addEventListener("change", () => {
+  synth.set({
+    envelope: {
+      attack: attackInput.value,
+    },
+  });
+});
+
+decayInput.addEventListener("change", () => {
+  synth.set({
+    envelope: {
+      decay: decayInput.value,
+    },
+  });
+});
+
+sustainInput.addEventListener("change", () => {
+  synth.set({
+    envelope: {
+      sustain: sustainInput.value,
+    },
+  });
+});
+
+releaseInput.addEventListener("change", () => {
+  synth.set({
+    envelope: {
+      release: releaseInput.value,
+    },
+  });
+});
+
+frequencyInput.addEventListener("change", () => {
+  chorus.frequency.value = frequencyInput.value;
+});
+
+delayTimeInput.addEventListener("change", () => {
+  chorus.delayTime = delayTimeInput.value;
+});
+
+depthInput.addEventListener("change", () => {
+  chorus.depth = depthInput.value;
+});
+
+buttonC.addEventListener("click", async () => {
+  await Tone.start();
+  synth.triggerAttackRelease(["C3", "E3"], "4n");
+});
+
+buttonD.addEventListener("click", () => {
+  synth.triggerAttackRelease(["D3", "F3"], "4n");
+});
+
+buttonE.addEventListener("click", () => {
+  synth.triggerAttackRelease(["E3", "G3"], "4n");
+});
+
+buttonF.addEventListener("click", () => {
+  synth.triggerAttackRelease(["F3", "A3"], "4n");
+});
+
+buttonG.addEventListener("click", () => {
+  synth.triggerAttackRelease(["G3", "B3"], "4n");
+});
+
+buttonA.addEventListener("click", () => {
+  synth.triggerAttackRelease(["A3", "C4"], "4n");
+});
+
+buttonB.addEventListener("click", () => {
+  synth.triggerAttackRelease(["B3", "D4"], "4n");
+});
+
+buttonC2.addEventListener("click", () => {
+  synth.triggerAttackRelease(["C4", "E4"], "4n");
+});
+
+window.addEventListener("click", () => {
+  Tone.start().then(() => console.log("audio started"));
+});
